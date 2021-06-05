00418898dcab8a1f575a80b178550f02aa911ebb
krixtan <krix.tam@qq.com>
2021-06-06 01:45:42 +0800

    规范化ImageFileElf的schema

--- ImageFileElf.py
+++ ImageFileElf.py
@@ -15,30 +15,60 @@ class ImageFileElf(DataFileElf):
         self._config = config({
             'name': 'ImageFileElf',
             'default': {
-                'input': 'input_filename',
-                'output': 'output_filename',
-                'favicon_size': 32,
+                'favicon': {
+                    'size': -1,
+                    'input': 'input_filename'
+                },
                 'splice': {
+                    'output': 'output_filename',
                     'images': [],
                     'width': 700,
                     'gap': 5
+                },
+                'watermark': {
+                    'input': 'input_filename',
+                    'output': 'output_filename',
+                    'text': 'Krix.Tam',
+                    'color': '000',
+                    'font': 'arial.ttf',
+                    'font_size': 24,
+                    'x': 5,
+                    'y': 5
                 }
             },
             'schema': {
                 'type': 'object',
                 'properties': {
-                    'input': {'type': 'string'},
-                    'output': {'type': 'string'},
-                    'favicon_size': {'type': 'number'},
+                    'favicon': {
+                        'type': 'object',
+                        'properties': {
+                            'size': {'type': 'number'},
+                            'input': {'type': 'string'},
+                        }
+                    },
                     'splice': {
                         'type': 'object',
                         'properties': {
+                            'output': {'type': 'string'},
                             'images': {
                                 'type': 'array',
                                 'items': {'type': 'string'}
                             },
                             'width': {'type': 'number'},
                             'gap': {'type': 'number'}
+                        }
+                    },
+                    'watermark': {
+                        'type': 'object',
+                        'properties': {
+                            'input': {'type': 'string'},
+                            'output': {'type': 'string'},
+                            'text': {'type': 'string'},
+                            'color': {'type': 'string'},
+                            'font': {'type': 'string'},
+                            'font_size': {'type': 'number'},
+                            'x': {'type': 'number'},
+                            'y': {'type': 'number'}
                         }
                     }
                 }
@@ -47,19 +77,22 @@ class ImageFileElf(DataFileElf):
 
     def to_favicon(self, **kwargs):
         self.set_config(**kwargs)
-        input_filename = self._config['input']
-        img = Image.open(self.get_filename_with_path(input_filename))
-        if self._config.is_default('favicon_size'):
-            icon_sizes = [16, 24, 32, 48, 64, 128, 255]
-            for x in icon_sizes:
-                img_resize = img.resize((x, x), Image.ANTIALIAS)
-                output_filename = 'favicon' + str(x) + '.ico'
-                img_resize.save(self.get_output_path(output_filename))
+        if self._config.is_default('favicon'):
+            logging.warning('"favicon"没有设置正确，请设置后重试。')
         else:
-            favicon_size = self._config['favicon_size']
-            img_resize = img.resize((favicon_size, favicon_size), Image.ANTIALIAS)
-            output_filename = 'favicon' + str(favicon_size) + '.ico'
-            img_resize.save(self.get_output_path(output_filename))
+            input_filename = self._config['favicon']['input']
+            img = Image.open(self.get_filename_with_path(input_filename))
+            if self._config.is_default(['favicon', 'size']):
+                icon_sizes = [16, 24, 32, 48, 64, 128, 255]
+                for x in icon_sizes:
+                    img_resize = img.resize((x, x), Image.ANTIALIAS)
+                    output_filename = 'favicon' + str(x) + '.ico'
+                    img_resize.save(self.get_output_path(output_filename))
+            else:
+                favicon_size = self._config['favicon']['size']
+                img_resize = img.resize((favicon_size, favicon_size), Image.ANTIALIAS)
+                output_filename = 'favicon' + str(favicon_size) + '.ico'
+                img_resize.save(self.get_output_path(output_filename))
 
     def splice(self, **kwargs):
         self.set_config(**kwargs)
@@ -67,7 +100,7 @@ class ImageFileElf(DataFileElf):
             logging.warning('"splice"没有设置正确，请设置后重试。')
         else:
             num_img = len(self._config['splice']['images'])
-            output_filename = self._config['output']
+            output_filename = self._config['splice']['output']
             if num_img > 0:
                 width = self._config['splice']['width']
                 gap = self._config['splice']['gap']
--- tests/test_ImageFileElf.py
+++ tests/test_ImageFileElf.py
@@ -9,14 +9,22 @@ class TestImageFileElf(unittest.TestCase):
 
     def test_to_favicon(self):
         input_filename = os.path.join('sources', 'icon.png')
+        favicon_settings = {
+            'input': input_filename,
+            'size': -1
+        }
         df_elf = ImageFileElf()
-        df_elf.to_favicon(input=input_filename)
+        df_elf.to_favicon(favicon=favicon_settings)
         icon_sizes = [16, 24, 32, 48, 64, 128, 255]
         for x in icon_sizes:
             filename = 'favicon' + str(x) + '.ico'
             result_filename = os.path.join('result', 'IFE', filename)
             self.assertEqual(df_elf.checksum(df_elf.get_output_path(filename)), df_elf.checksum(df_elf.get_filename_with_path(result_filename)))
-        df_elf.to_favicon(input=input_filename, favicon_size=192)
+        favicon_settings = {
+            'input': input_filename,
+            'size': 192
+        }
+        df_elf.to_favicon(favicon=favicon_settings)
         filename = 'favicon192.ico'
         result_filename = os.path.join('result', 'IFE', filename)
         self.assertEqual(df_elf.checksum(df_elf.get_output_path(filename)),
@@ -24,15 +32,15 @@ class TestImageFileElf(unittest.TestCase):
 
     def test_splice(self):
         input_filename = os.path.join('sources', 'icon.png')
-        print(input_filename)
+        filename = 'icon_splice.png'
         df_elf = ImageFileElf()
         splice_config = {
+            'output': filename,
             'images': [input_filename, input_filename],
             'width': 125,
             'gap': 5
         }
-        filename = 'icon_splice.png'
-        df_elf.splice(output=filename, splice=splice_config)
+        df_elf.splice(splice=splice_config)
         result_filename = os.path.join('result', filename)
         self.assertEqual(df_elf.checksum(df_elf.get_output_path(filename)),
                          df_elf.checksum(df_elf.get_filename_with_path(result_filename)))
